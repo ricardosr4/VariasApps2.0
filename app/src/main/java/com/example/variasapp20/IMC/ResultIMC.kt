6cd9cad0e35898d5199e42b57b3c10fff1b67374
ricardo soto <ricardosr4@gmail.com>
2023-08-25 08:52:46 -0400

    todo ok

---
+++ app/src/main/java/com/example/variasapp20/IMC/ResultIMC.kt
@@ -0,0 +1,12 @@
+package com.example.variasapp20.IMC
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.variasapp20.R
+
+class ResultIMC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_imc)
+    }
+}
